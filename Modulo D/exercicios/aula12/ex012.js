ce9383aaa1273cc57dd021456194361593b45227
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-06-29 20:03:44 -0300

    Create ex012.js

---
+++ Modulo D/exercicios/aula12/ex012.js
@@ -0,0 +1,13 @@
+//EXERCICIO 012 - CONDIÇÃO ANINHADA
+var agora = new Date(); //DATA E HORA CAPTURADA DO LADO DO SERVIDOR
+var horas = agora.getHours(); //RETIRAR SOMENTE AS HORAS DA VARIAVEL CAPTURADA
+
+console.log(`Agora são exatamente ${horas} horas`);
+
+if(horas < 12){
+    console.log(`Bom dia!`);
+}else if(horas <= 18){
+    console.log(`Boa tarde`);
+}else{
+    console.log(`Boa noite!`)
+}
