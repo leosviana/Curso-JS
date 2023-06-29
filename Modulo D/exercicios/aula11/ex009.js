997fef59d4fbc1f1914cafa04b1fa7df1e30decf
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-06-29 19:14:57 -0300

    Create ex009.js

---
+++ Modulo D/exercicios/aula11/ex009.js
@@ -0,0 +1,9 @@
+//EXERCICIO 009 - CONDIÇÃO COMPOSTA
+var pais = 'EUA';
+
+console.log(`Vivendo em ${pais}`)
+if (pais == 'BRASIL'){
+    console.log('Sou Brasileiro!');
+}else{
+    console.log('Sou estrangeiro!');
+}
