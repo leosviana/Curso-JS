976b44b5d089cddddf748c0a0c71bb7c1df08813
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-06-29 19:14:48 -0300

    Create ex008.js

---
+++ Modulo D/exercicios/aula11/ex008.js
@@ -0,0 +1,10 @@
+//EXERCICIO 008 - CONDIÇÃO SIMPLES
+console.log('O console funcionou corretamente!');
+
+var velocidade = 40.5
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`);
+
+if(velocidade > 60){ 
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+console.log('Dirija sempre usando cinto de segurança!');
