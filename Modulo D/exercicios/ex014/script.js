3b9903c79a47e011dad680885458f44000da4660
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-02 12:17:34 -0300

    Update script.js

--- Modulo D/exercicios/ex014/script.js
+++ Modulo D/exercicios/ex014/script.js
@@ -6,7 +6,8 @@ function carregar(){
     var imagem = window.document.getElementById('imagem');
     var data = new Date(); //DATA E HORA CAPTURADA DO LADO DO SERVIDOR
     var hora = data.getHours(); //RETIRA SOMENTE AS HORAS DA VARIAVEL "data" CAPTURADA
-    msg.innerHTML = `Agora são ${hora} horas.`; //ALTERA MENSAGEM DA DIV "msg"
+    var min = data.getMinutes();
+    msg.innerHTML = `Agora são ${hora}:${min} hs.`; //ALTERA MENSAGEM DA DIV "msg"
 
     if(hora >= 0 && hora < 12){ //BOM DIA = ENTRE 00:00 HORAS ATÉ 11:59 HORAS
         imagem.src = 'foto-manha.png'; //CARREGA FOTO NA TAG "img"
