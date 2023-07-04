d363afea132dfe7e4b4b87bd2bf3388737fc46d4
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-04 12:40:33 -0300

    Create ex016.js

---
+++ Modulo E/exercicios/aula13/ex016.js
@@ -0,0 +1,15 @@
+//EXERCICIO 016 - ESTRUTURA DE REPETIÇÃO
+
+//ESTRUTURA WHILE
+var cont = 1; //CONTADOR
+while(cont <= 6){ //TESTA PRIMEIRO, DEPOIS EXECUTA O BLOCO
+    console.log(`Passo ${cont}`);
+    cont++; //CONTADOR + 1. NUNCA ESQUECER ESSE COMANDO. POIS É A "FINALIZAÇÃO" DO LOOP
+}
+
+//ESTRUTURA DO..WHILE
+var cont2 = 1; //CONTADOR
+do{ //EXECUTA O BLOCO PRIMEIRO, TESTA DEPOIS NO FINAL
+    console.log(`Passo ${cont2}`);
+    cont2++; //CONTADOR + 1. NUNCA ESQUECER ESSE COMANDO. POIS É A "FINALIZAÇÃO" DO LOOP
+}while(c <= 6);
