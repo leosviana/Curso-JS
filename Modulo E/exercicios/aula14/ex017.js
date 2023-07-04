18eeea28657db8cec0da16185ab1673cc6cacff4
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-04 12:40:54 -0300

    Create ex017.js

---
+++ Modulo E/exercicios/aula14/ex017.js
@@ -0,0 +1,7 @@
+//EXERCICIO 017 - ESTRUTURA DE REPETIÇÃO COM VARIÁVEL DE CONTROLE
+
+console.log('Vai começar...');
+for(var cont = 1; cont <= 6; cont++){
+    console.log(`Passo ${cont}`);
+}
+console.log('FIM!');
