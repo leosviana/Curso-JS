36637cad804636d3b6b0a67fdb1d65aff7f0af6e
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-06 00:33:41 -0300

    Create tmp.js

---
+++ Modulo E/exercicios/aula14/ex018/node_b9728e54f671a.tmp.js
@@ -0,0 +1,7 @@
+ var inicio = 1;
+    var fim = 3;
+    var passo = 1;
+    for(var passo = inicio; inicio <= fim; passo++){
+        // res.innerHTML = `${inicio}`;
+        console.log(`${inicio} e ${fim} e ${passo}`);
+    }
