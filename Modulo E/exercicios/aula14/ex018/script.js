ed124e627a3dd078defeea1e200788de6eb8a8d7
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-06 23:26:55 -0300

    Update script.js

--- Modulo E/exercicios/aula14/ex018/script.js
+++ Modulo E/exercicios/aula14/ex018/script.js
@@ -6,15 +6,17 @@ function contar(){
     let res = window.document.getElementById('res');
 
     if(inicio.value.lenght == '' || fim.value.lenght == '' || passo.value.lenght == ''){
-        window.alert('[ERRO] Insira os dados corretamente!');
+        // window.alert('[ERRO] Insira os dados corretamente!');
         res.innerHTML = 'Impossível contar!';
-    }else if(passo <= 0){
-        passo = 1;
     }else{
-        res.innerHTML = `\u{1F913} Contando: `;
+        res.innerHTML = `\u{1F913} Contando: <br>`;
         let i = Number(inicio.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
+        if(passo <= 0){
+            window.alert('Passo inválido, considerando passo 1');
+            passo = 1;
+        }
 
         if(i < f){ //CONTAGEM CRESCENTE
             for(let cont = i; cont <= f; cont+=p){              
@@ -22,10 +24,9 @@ function contar(){
             }
         }else{ //SE NÃO CONTAGEM DECRESCENTE
             for(let cont = i; cont >= f; cont-=p){              
-                res.innerHTML += `\u{1F449}${cont}, `;        
+                res.innerHTML += `\u{1F449}${cont}, `;  
             }
         }
         res.innerHTML += `\u{1F3C1}`;
     }
-
 }
