f70031b3b47069688959627d8e760b46a7f88705
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-06 23:17:59 -0300

    Update script.js

--- Modulo E/exercicios/aula14/ex018/script.js
+++ Modulo E/exercicios/aula14/ex018/script.js
@@ -5,26 +5,27 @@ function contar(){
     let passo = window.document.getElementById('passo');
     let res = window.document.getElementById('res');
 
-    if(inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+    if(inicio.value.lenght == '' || fim.value.lenght == '' || passo.value.lenght == ''){
         window.alert('[ERRO] Insira os dados corretamente!');
-    }else if(inicio.value.lenght == '' || fim.value.lenght == '' || passo.value.lenght == ''){
-        window.alert('[ERRO] Insira os dados corretamente!');
-    }else if(passo == 0){
+        res.innerHTML = 'Impossível contar!';
+    }else if(passo <= 0){
         passo = 1;
     }else{
-        res.innerHTML = `Contando: `;
+        res.innerHTML = `\u{1F913} Contando: `;
         let i = Number(inicio.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
 
-        for(let cont = i; cont <= f; cont+=p){
-            if(cont >= f){
-                res.innerHTML += `${cont}.`;
-            }else{
-                res.innerHTML += `${cont}, `;
+        if(i < f){ //CONTAGEM CRESCENTE
+            for(let cont = i; cont <= f; cont+=p){              
+                res.innerHTML += `\u{1F449}${cont}, `;        
             }
-            
+        }else{ //SE NÃO CONTAGEM DECRESCENTE
+            for(let cont = i; cont >= f; cont-=p){              
+                res.innerHTML += `\u{1F449}${cont}, `;        
+            }
         }
+        res.innerHTML += `\u{1F3C1}`;
     }
 
 }
