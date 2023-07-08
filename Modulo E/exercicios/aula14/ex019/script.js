fa1d209c9f58352be98358da18cb008cccda0dc7
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-08 20:08:21 -0300

    Update script.js

--- Modulo E/exercicios/aula14/ex019/script.js
+++ Modulo E/exercicios/aula14/ex019/script.js
@@ -1,1 +1,11 @@
-var tab = window.
+function calcular(){
+    let num = window.document.getElementById('tabuada');
+    let tab = window.document.getElementById('seltab');
+
+    for(cont = num; cont <= num; cont++){
+        let item = document.createElement('option');
+        item.text = `${num} x ${con} = ${num*cont}`;
+        tab.appendChild(item);
+        // res.innerHTML += `${Number(tab.value)} x ${Number(cont.value)} = ${r}`;
+    }
+}
