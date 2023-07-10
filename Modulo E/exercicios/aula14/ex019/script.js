5072ad96fe5f0c793a02bf728a7e0449795b9ce7
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-09 23:02:50 -0300

    Update script.js

--- Modulo E/exercicios/aula14/ex019/script.js
+++ Modulo E/exercicios/aula14/ex019/script.js
@@ -2,10 +2,18 @@ function calcular(){
     let num = window.document.getElementById('tabuada');
     let tab = window.document.getElementById('seltab');
 
-    for(cont = num; cont <= num; cont++){
-        let item = document.createElement('option');
-        item.text = `${num} x ${con} = ${num*cont}`;
-        tab.appendChild(item);
-        // res.innerHTML += `${Number(tab.value)} x ${Number(cont.value)} = ${r}`;
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!');
+    }
+    else{
+        let n = Number(num.value); //TRANSFORMANDO VARIAVEL EM NUMERO
+        tab.innerHTML = ''; //LIMPAR CAMPO ANTES DE MOSTRAR O PROXIMO RESULTADO
+
+        for(cont = 1; cont <= 10; cont++){ //CONTAGEM DE 1 ATÉ 10
+            let item = document.createElement('option'); //CRIANDO OS ITENS EM FORMA DE RESULTADO DENTRO DO SELECT DO HTML
+            item.text = `${n} x ${cont} = ${n*cont}`; //RESULTADO = NUMERO * X
+            //item.value = `tab${c}`; //USADO PARA ENUMERAR CADA LINHA DO RESULTADO. USANDO APENAS EM PHP
+            tab.appendChild(item); //EXIBINDO OS ITENS DENTRO DO SELECT DO HTML
+        }
     }
 }
