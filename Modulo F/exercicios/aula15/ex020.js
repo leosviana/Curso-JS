eab55c7e2c6cb658ba874b5428f80b7a8a690ba8
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-10 00:40:42 -0300

    Update ex020.js

--- Modulo F/exercicios/aula15/ex020.js
+++ Modulo F/exercicios/aula15/ex020.js
@@ -1,8 +1,10 @@
 let num = [3, 4, 8, 6, 9, 1];
 
-console.log(`Nosso vetor é ${num}`);
-console.log(`O veto tem um ${num.length} posições`);
-console.log(`O valor do elemento 0 é: ${num[0]}`);
-console.log(`Adicionando o valor 5 no ultimo elemento ${num.push(5)}`);
-console.log(`Nosso vetor com o valor inserido agora é: ${num}`);
-console.log(`O vetor ordenado é: ${num.sort()}`);
+console.log(`Nosso vetor é ${num}.`);
+console.log(`O veto tem ${num.length} posições.`);
+console.log(`O valor do elemento 0 é: ${num[0]}.`);
+console.log(`Adicionando o valor 5 no ultimo elemento ${num.push(5)}.`);
+console.log(`Nosso vetor com o valor inserido agora é: ${num}.`);
+console.log(`A posição ${num.indexOf(8)} está com o valor 8`);
+console.log(`O valor 2 não existe, portanto o resultado de posição para o JavaScript é ${num.indexOf(2)} `);
+console.log(`O vetor ordenado é: ${num.sort()}.`);
