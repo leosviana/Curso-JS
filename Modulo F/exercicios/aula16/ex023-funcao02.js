2ce956741d1eff3dea6c27474cca597e26e0161f
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-13 23:37:53 -0300

    Create ex023-funcao01.js

---
+++ Modulo F/exercicios/aula16/ex023-funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0){
+    //n1=0 SIGNIFICA QUE SE NÃO FOR PASSADO PARAMATRO, ASSUMIRÁ VALOR 0
+    return n1 + n2; //SOMA VALORS
+}
+
+console.log(soma(2,7)); //ENVIA O N1 E N2 PARA EXECUTAR NA FUNÇÃO, E JÁ EXIBE O VALOR EM TELA
