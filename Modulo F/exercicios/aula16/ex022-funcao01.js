bc9b751690a4f58a3c1c2d67f4cee48eac01b4ad
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-13 23:37:43 -0300

    Create ex022-funcao01.js

---
+++ Modulo F/exercicios/aula16/ex022-funcao01.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if(n%2 == 0){
+        return 'PAR!';
+    }else{
+        return 'IMPAR!';
+    }
+}
+
+//let res = parimpar(4);
+//console.log(res);
+//OU
+console.log(parimpar(4));
