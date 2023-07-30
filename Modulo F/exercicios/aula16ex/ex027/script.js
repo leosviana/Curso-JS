90713115cd1e0aae7611e576f47541a145c26aad
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-07-30 20:52:11 -0300

    Update script.js

--- Modulo F/exercicios/aula16ex/ex027/script.js
+++ Modulo F/exercicios/aula16ex/ex027/script.js
@@ -47,9 +47,9 @@ function finalizar(){
             if (valores[pos] < menor)
                 menor = valores[pos]
         }
-        //res.innerHTML = ''        
+        res.innerHTML = '';      
         res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
-            res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
     }
 }
